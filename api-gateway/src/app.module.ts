d27078f613713f1dc35d00dfaa9126b2f2061783
Rupesh Shandilya <rupeshshandilya@Rupeshs-MacBook-Pro.local>
2024-07-14 22:46:10 +0530

    removed unnecessary files

--- api-gateway/src/app.module.ts
+++ api-gateway/src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ClientsModule, Transport } from '@nestjs/microservices';
 import { AuthModule } from './auth.module';
 import { ConfigModule } from '@nestjs/config';
@@ -29,6 +28,6 @@ import { ConfigModule } from '@nestjs/config';
     ]),
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
